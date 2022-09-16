fc9fa17f3aa9c347397222818924155741634f5d
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2022-09-17 00:20:46 +0300

    Initial Commit

---
+++ iOSColorPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSColorPicker
+//
+//  Created by Artem Goldenberg on 17.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSColorPicker/iOSColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSColorPickerApp.swift
+//  iOSColorPicker
+//
+//  Created by Artem Goldenberg on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
